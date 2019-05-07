b140dcdb745ca353746fbe54f587ade29dbaa4fa
Pontus Asp <pontus.asp@gmail.com>
2019-05-07 16:55:05 +0200

    Made user load data from store
    Fixed bio resizing issue
    Made user load data from store

--- src/modules/user/_view/User.vue
+++ src/modules/user/_view/User.vue
@@ -4,25 +4,20 @@
 
 <script>
 import UserProfile from '@/modules/user/_components/UserProfile';
+import store from "@/store";
+import { mapState, mapActions } from "vuex";
 
 export default {
   name: "user",
+  beforeRouteEnter(to, from, next) {
+    store.dispatch("getUser", to.params.uuid).then(() => 
+    next());
+  },
+  computed: mapState({
+    user: state => state.user.user
+  }),
   components: {
     UserProfile
-  },
-  data() {
-    return {
-      user: {
-        user_display_name: "Pontus Asp",
-        username: "Pontusblue",
-        avatar_url:
-          "https://avatars2.githubusercontent.com/u/18573650?s=460&v=4",
-        user_email: "poasp@kth.se",
-        profile_url: "https://github.com/Pontusblue",
-        user_bio: "I am master of faster lord of distaster, sanic's my name, speed's my game",
-        user_role: "Master",
-      }
-    };
   }
 };
 </script>
